f26e7e13dee83857322f27b6049ae30ec7667634
David Wendland <the.devicenator@gmail.com>
2021-07-23 21:08:16 +0200

    Add KeyboardWatcher to listen for global keyboard events with the application not in focus.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
 ## [Ideas/Upcoming]
 Keyboard Navigation (Shortcuts)
-KeyboardWatcher
+Mouse Gestures
 
 ## [Unreleased]
-
+### Added
+* KeyboardWatcher to listen for global keyboard events with the application not in focus.
---
+++ SniffCore.Shortcuts/Internal/KeyboardWatcherCallback.cs
@@ -0,0 +1,20 @@
+//
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace SniffCore.Shortcuts.Internal
+{
+    internal class KeyboardWatcherCallback
+    {
+        internal KeyboardWatchToken Token { get; set; }
+
+        internal Action<KeyStateChangedArgs> Callback { get; set; }
+        internal IEnumerable<Key> Keys { get; set; }
+        internal IEnumerable<KeyState> KeyStates { get; set; }
+    }
+}
